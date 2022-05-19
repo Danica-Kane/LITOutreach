a42b5d5fcc2ad2d55ace0b0099f3936fc9665d35
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2022-05-19 22:16:12 +1200

    commit

--- LITOutreach-v1-19:5:22/index.js
+++ LITOutreach-v1-19:5:22/index.js
@@ -2,26 +2,27 @@ document.getElementById("FontFamily").onclick = changeFontFamily;
 
 //set localstorage values once only, if not set
 if (!localStorage.getItem('buttonVar')) {
-    localStorage.setItem('buttonVar', "Normal");
+    localStorage.setItem('buttonVar', "Dyslexia Font");
     localStorage.setItem('bodyVar',"Lato, sans-serif" );
 }
 
 //set the initial css rules 
 document.body.style.fontFamily = localStorage.getItem('bodyVar');
 document.getElementById("buttonFont").innerHTML = localStorage.getItem('buttonVar');
+document.getElementById("buttonFont").innerHTML = "Dyslexia Font";
 
 
 
 function changeFontFamily() {
   if (localStorage.getItem("buttonVar") == "Normal") {
     document.body.style.fontFamily = "OpenDyslexic3Regular";
-    document.getElementById("buttonFont").innerHTML = "Dyslexia Font";
+    document.getElementById("buttonFont").innerHTML = "Normal Font";
     localStorage.setItem('buttonVar', "Dyslexia Font");
     localStorage.setItem('bodyVar',"OpenDyslexic3Regular" );
    
   } else {
     document.body.style.fontFamily = "Lato, sans-serif";
-    document.getElementById("buttonFont").innerHTML = "Normal";
+    document.getElementById("buttonFont").innerHTML = "Dyslexia Font";
     localStorage.setItem('buttonVar', "Normal");
     localStorage.setItem('bodyVar',"Lato, sans-serif" );
    
