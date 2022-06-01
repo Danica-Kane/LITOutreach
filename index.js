41288ea154b08796204df85cfa98f5eb0cb2aabc
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2022-06-01 12:41:17 +1200

    commit

--- index.js
+++ index.js
@@ -51,3 +51,17 @@ if (alertTrigger) {
 }
 
 //nameInput != null && emailInput != null && workInput != null && descriptionOfWork != null;
+
+//TESTING
+let input = document.querySelector(".input");
+let button = document.querySelector(".button");
+button.disabled = true;
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if(document.querySelector(".input").value === "") {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+}
